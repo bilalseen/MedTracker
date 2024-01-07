08a755c9d29fcd1e6497988a183516ae3a05ada3
bilalseen <bilalsen2010@gmail.com>
2024-01-07 21:35:02 +0300

    Add, Home, Profile pages created

---
+++ src/pages/Add/Add.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from "./Add.style"
+
+
+const Add = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Add Page!</Text>
+        </View>
+    )
+}
+
+export default Add
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from "./Add.style"
+
+
+const Home = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Home Page!</Text>
+        </View>
+    )
+}
+
+export default Home
