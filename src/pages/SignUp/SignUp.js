ea9513d4b582255f70afb9d7ab10c1b742988f82
bilalseen <bilalsen2010@gmail.com>
2024-01-07 13:35:22 +0300

    Auth pages created

---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from "./SignUp.style"
+const SignUp = () => {
+    return (
+        <View style={styles.container}>
+            <Text>SignUp Pagee!!!</Text>
+        </View>
+    )
+}
+
+export default SignUp
