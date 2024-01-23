e74176bf2fad934d42cbda64ecbf07c8bd283821
bilalseen <bilalsen2010@gmail.com>
2024-01-23 21:21:32 +0300

    props added

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,8 +2,14 @@ import { View, Text, TextInput } from "react-native";
 import React from "react";
 import styles from "./SearchBar.style";
 
-const SearchBar = () => {
-  return <TextInput style={styles.input} />;
+const SearchBar = ({ placeholder, onSearch }) => {
+  return (
+    <TextInput
+      style={styles.input}
+      placeholder={placeholder}
+      onChangeText={onSearch}
+    />
+  );
 };
 
 export default SearchBar;
