fb11d18aa1ae55e79e0bae0f08d537c6f3dce1bb
bilalseen <bilalsen2010@gmail.com>
2024-01-23 21:19:38 +0300

    style class defined

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,9 @@
+import { View, Text, TextInput } from "react-native";
+import React from "react";
+import styles from "./SearchBar.style";
+
+const SearchBar = () => {
+  return <TextInput style={styles.input} />;
+};
+
+export default SearchBar;
