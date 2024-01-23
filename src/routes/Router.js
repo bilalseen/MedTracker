b939420257425db8834dc1d741caecfbc74c604f
bilalseen <bilalsen2010@gmail.com>
2024-01-23 20:54:39 +0300

    name changed

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,54 +1,40 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TabNavigation from "../routes/TabNavigation";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
-import Add from "../pages/Add";
-import Home from "../pages/Home";
-import Profile from "../pages/Profile";
+const Stack = createNativeStackNavigator();
 
-const Tab = createBottomTabNavigator();
-
-function App() {
-    return (
-
-        <Tab.Navigator
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
-
-                    if (route.name === "Home") {
-                        iconName = focused ? "home" : "home-outline";
-                    } else if (route.name === "Add") {
-                        iconName = focused ? "add-circle" : "add-circle-outline";
-                    } else if (route.name === "Profile") {
-                        iconName = focused ? "person" : "person-outline";
-                    }
-
-                    // İkonu döndür
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                },
-                tabBarActiveTintColor: "#505050",
-                tabBarInactiveTintColor: "#BDBDC7",
-            })}
-        >
-            <Tab.Screen
-                name="Home"
-                component={Home}
-                options={{ headerShown: false }}
-            />
-            <Tab.Screen
-                name="Add"
-                component={Add}
-                options={{ headerShown: false }}
-            />
-            <Tab.Screen
-                name="Profile"
-                component={Profile}
-                options={{ headerShown: false }}
-            />
-        </Tab.Navigator>
-
-    );
+function Router() {
+  const [user, setUser] = useState(null);
+  return (
+    <NavigationContainer>
+      {user ? (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="TabNavigation"
+            component={TabNavigation}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
+  );
 }
 
-export default App;
+export default Router;
