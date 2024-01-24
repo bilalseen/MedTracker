92d251155dc52c958965e04d8df16b246f8b5ca1
bilalseen <bilalsen2010@gmail.com>
2024-01-24 14:58:14 +0300

    page edited

--- src/pages/Add/Add.js
+++ src/pages/Add/Add.js
@@ -1,14 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import styles from "./Add.style"
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import moment from "moment";
 
+import styles from "./Add.style";
+
+import InputBar from "../../components/InputBar";
+import CustomCalendar from "../../components/CustomCalendar";
+import CustomButton from "../../components/CustomButton";
 
 const Add = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Add Page!</Text>
-        </View>
-    )
-}
+  const [buttonColor, setButtonColor] = useState("gray");
+  const [inputText, setInputText] = useState("");
+  const [selectedDate, setSelectedDate] = useState("");
+  const today = moment();
 
-export default Add
+  //button background color set
+  useEffect(() => {
+    inputText.length > 0 && selectedDate.length > 0
+      ? setButtonColor("orange")
+      : setButtonColor("gray");
+  }, [inputText, selectedDate]);
+
+  //calendar select date set process
+  const handleDateChange = (date) => {
+    const formattedDate = moment(date).format("YYYY-MM-DD").toString();
+    setSelectedDate(formattedDate);
+  };
+
+  return (
+    <View style={styles.container}>
+      <InputBar
+        placeholder={"Medicine name"}
+        onChange={(text) => setInputText(text)}
+      />
+      <CustomCalendar onDateChange={handleDateChange} />
+      <CustomButton
+        buttonText={"Save"}
+        backgroundColor={buttonColor}
+        onPress={() => {
+          console.log("Save button pressed");
+        }}
+      />
+    </View>
+  );
+};
+
+export default Add;
