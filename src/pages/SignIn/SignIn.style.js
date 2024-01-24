5e53ca52acd225b9d1d7f3270013792e58a35ad5
bilalseen <bilalsen2010@gmail.com>
2024-01-24 21:26:36 +0300

    style added

--- src/pages/SignIn/SignIn.style.js
+++ src/pages/SignIn/SignIn.style.js
@@ -1,9 +1,25 @@
 import { StyleSheet } from "react-native";
 
 export default styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    alignItems: "center",
+    gap: 20,
+    marginBottom: 40,
+  },
+  icon: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  headerText: {
+    fontSize: 20,
+  },
+  titleText: {
+    fontSize: 14,
+  },
+});
