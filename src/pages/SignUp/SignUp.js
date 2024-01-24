e2fcb6e4312bb60369b91a50fe1c7c1559648e4a
bilalseen <bilalsen2010@gmail.com>
2024-01-24 19:57:13 +0300

    custom line added

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -2,10 +2,17 @@ import { View } from "react-native";
 import React from "react";
 import styles from "./SignUp.style";
 import SignUpForm from "../../components/SignUpForm";
+import CustomLine from "../../components/CustomLine";
 const SignUp = () => {
   return (
     <View style={styles.container}>
       <SignUpForm gap={25} />
+      <CustomLine
+        text={"Or"}
+        textColor={"gray"}
+        borderColor={"gray"}
+        width={300}
+      />
     </View>
   );
 };
