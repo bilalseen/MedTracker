966bfcb2f8ee519fc310113e00d034be3ab9620d
bilalseen <bilalsen2010@gmail.com>
2024-01-24 19:57:47 +0300

    background image added

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, ImageBackground } from "react-native";
 import React from "react";
 import styles from "./SignUp.style";
 import SignUpForm from "../../components/SignUpForm";
@@ -6,16 +6,21 @@ import CustomLine from "../../components/CustomLine";
 import GoogleAuthButton from "../../components/GoogleAuthButton";
 const SignUp = () => {
   return (
-    <View style={styles.container}>
-      <SignUpForm gap={25} />
-      <CustomLine
-        text={"Or"}
-        textColor={"gray"}
-        borderColor={"gray"}
-        width={300}
-      />
-      <GoogleAuthButton />
-    </View>
+    <ImageBackground
+      source={require("../../../assets/background-image-opacity-15.png")}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <View style={styles.container}>
+        <SignUpForm gap={25} />
+        <CustomLine
+          text={"Or"}
+          textColor={"gray"}
+          borderColor={"gray"}
+          width={300}
+        />
+        <GoogleAuthButton />
+      </View>
+    </ImageBackground>
   );
 };
 
