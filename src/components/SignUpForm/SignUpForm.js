ff5ae2c59d2a5d9416243425b78af19a95e34aeb
bilalseen <bilalsen2010@gmail.com>
2024-01-25 14:26:01 +0300

    firebase sign up defined

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -4,11 +4,33 @@ import styles from "./SignUpForm.style";
 import { Formik } from "formik";
 import AuthInput from "../AuthInput";
 import CustomButton from "../CustomButton";
+import FIREBASE_AUTH from "../../services/config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const signUpUser = (values) => {
+  const auth = FIREBASE_AUTH;
+  createUserWithEmailAndPassword(auth, values.email, values.password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      user.displayName = values.fullName;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+};
 const SignUpForm = ({ gap }) => {
+  const onSubmit = (values) => {
+    signUpUser(values);
+  };
   return (
     <Formik
       initialValues={{ fullName: "", email: "", password: "" }}
-      onSubmit={() => null}
+      onSubmit={onSubmit}
     >
       {({ handleChange, handleBlur, handleSubmit, values }) => (
         <View style={[styles.inputContainer, { gap: gap }]}>
