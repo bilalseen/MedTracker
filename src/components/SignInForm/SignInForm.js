cef57d23ed45ba157b5a82527c27f50613f18a48
bilalseen <bilalsen2010@gmail.com>
2024-01-25 14:26:12 +0300

    firebase sign in connected

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -4,10 +4,30 @@ import styles from "./SignInForm.style";
 import { Formik } from "formik";
 import AuthInput from "../AuthInput";
 import CustomButton from "../CustomButton";
+import FIREBASE_AUTH from "../../services/config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+const signInUser = (values) => {
+  const auth = FIREBASE_AUTH;
+  signInWithEmailAndPassword(auth, values.email, values.password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user.email);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
 
 const SignInForm = ({ gap }) => {
+  const onSubmit = (values) => {
+    signInUser(values);
+  };
   return (
-    <Formik initialValues={{ email: "", password: "" }} onSubmit={() => null}>
+    <Formik initialValues={{ email: "", password: "" }} onSubmit={onSubmit}>
       {({ handleChange, handleBlur, handleSubmit, values }) => (
         <View style={[styles.inputContainer, { gap: gap }]}>
           <AuthInput
