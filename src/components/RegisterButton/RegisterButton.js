cf48a3c5751b61f32a71c704e905c4779a75e4d4
bilalseen <bilalsen2010@gmail.com>
2024-01-25 14:13:59 +0300

    onPress connect the props

--- src/components/RegisterButton/RegisterButton.js
+++ src/components/RegisterButton/RegisterButton.js
@@ -2,9 +2,9 @@ import { TouchableWithoutFeedback, Text } from "react-native";
 import React from "react";
 import styles from "./RegisterButton.style";
 
-const RegisterButton = ({ text }) => {
+const RegisterButton = ({ text, onPress }) => {
   return (
-    <TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={onPress}>
       <Text style={styles.registerText}>{text}</Text>
     </TouchableWithoutFeedback>
   );
