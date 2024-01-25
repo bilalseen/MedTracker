43123c3f72bc6ea097e60b6d3e4d36e79c364c38
bilalseen <bilalsen2010@gmail.com>
2024-01-25 19:10:06 +0300

    loading and error animation added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,31 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Home.style";
 import SearchBar from "../../components/SearchBar";
+import LoadingAnimation from "../../components/LoadingAnimation";
+import ErrorAnimation from "../../components/ErrorAnimation";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    try {
+    } catch (error) {
+      console.error("Veri yükleme hatası:", error);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  if (loading) {
+    return <LoadingAnimation />;
+  }
+  if (error) {
+    return <ErrorAnimation />;
+  }
+
   return (
     <View style={styles.container}>
       <SearchBar placeholder={"Search med..."} onSearch={() => null} />
