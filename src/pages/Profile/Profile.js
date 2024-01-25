79f0bf7226712127b6cb8bb1a51140e0202f5626
bilalseen <bilalsen2010@gmail.com>
2024-01-25 18:06:21 +0300

    signout button added, firebase signout connected

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,14 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import styles from "./Profile.style"
-
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import styles from "./Profile.style";
+import CustomButton from "../../components/CustomButton";
+import FIREBASE_AUTH from "../../services/config";
+import { signOut } from "firebase/auth";
 
 const Profile = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Profile Page!</Text>
-        </View>
-    )
-}
+  const auth = FIREBASE_AUTH;
+  const [user, setUser] = useState(auth.currentUser);
 
-export default Profile
+  useEffect(() => {
+    setUser(auth.currentUser);
+    console.log(user);
+  }, [auth.currentUser]);
+
+  const userSignOut = () => {
+    const user = auth.currentUser;
+
+    if (user) {
+      signOut(auth)
+        .then(() => {
+          console.log("Sign-out successful.");
+        })
+        .catch((error) => {
+          console.log("An error happened.", error);
+        });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>{user.displayName}</Text>
+      <CustomButton
+        buttonText={"Log out"}
+        backgroundColor={"red"}
+        onPress={userSignOut}
+      />
+    </View>
+  );
+};
+
+export default Profile;
