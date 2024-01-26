eebf99be574fb8c6c7842bd12b2c6c1e845dca96
bilalseen <bilalsen2010@gmail.com>
2024-01-26 08:41:40 +0300

    value props added

--- src/components/InputBar/InputBar.js
+++ src/components/InputBar/InputBar.js
@@ -2,10 +2,14 @@ import React from "react";
 import { View, Text, TextInput } from "react-native";
 import styles from "./InputBar.style";
 
-const SearchBar = ({ placeholder, onChange }) => {
+const SearchBar = ({ placeholder, onChange, value }) => {
   return (
     <View style={styles.container}>
-      <TextInput placeholder={placeholder} onChangeText={onChange} />
+      <TextInput
+        placeholder={placeholder}
+        onChangeText={onChange}
+        value={value}
+      />
     </View>
   );
 };
