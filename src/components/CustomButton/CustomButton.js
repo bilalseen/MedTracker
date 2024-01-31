d6316fca262d31174343f40629509641441cb9b1
bilalseen <bilalsen2010@gmail.com>
2024-01-31 20:11:33 +0300

    width and height props added the component

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -2,11 +2,20 @@ import { Text, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import styles from "./CustomButton.style";
 
-const CustomButton = ({ buttonText, backgroundColor, onPress }) => {
+const CustomButton = ({
+  buttonText,
+  backgroundColor,
+  onPress,
+  width,
+  height,
+}) => {
   return (
     <TouchableOpacity
       onPress={onPress}
-      style={[styles.buttonContainer, { backgroundColor: backgroundColor }]}
+      style={[
+        styles.buttonContainer,
+        { backgroundColor: backgroundColor, width: width, height: height },
+      ]}
     >
       <Text style={styles.text}>{buttonText}</Text>
     </TouchableOpacity>
