1c0ffb57940d231fdeca6d7e7fe649cc30cd82dc
bilalseen <bilalsen2010@gmail.com>
2024-01-31 20:07:31 +0300

    component created

---
+++ src/pages/EditProfile/EditProfile.js
@@ -0,0 +1,15 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+
+const EditProfile = () => {
+  return (
+    <View>
+      <View>
+        <Image />
+        
+      </View>
+    </View>
+  );
+};
+
+export default EditProfile;
