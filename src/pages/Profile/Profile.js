9fc531b5a6a3f21ac6a3859c64947f5681893cf8
bilalseen <bilalsen2010@gmail.com>
2024-01-31 21:28:08 +0300

    page edited

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,18 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React, { useEffect, useState } from "react";
 import styles from "./Profile.style";
 import CustomButton from "../../components/CustomButton";
 import FIREBASE_AUTH from "../../services/config";
 import { signOut } from "firebase/auth";
+import MedData from "../../../data.json";
+import AccountButton from "../../components/AccountButton";
+import ToggleButton from "../../components/ToggleButton";
+import MaterialIcons from "../../components/MaterialIcons";
 
-const Profile = () => {
+const Profile = ({ navigation }) => {
   const auth = FIREBASE_AUTH;
   const [user, setUser] = useState(auth.currentUser);
 
   useEffect(() => {
     setUser(auth.currentUser);
-    console.log(user);
-  }, [auth.currentUser]);
+  }, [auth.currentUser, MedData, user.displayName]);
 
   const userSignOut = () => {
     const user = auth.currentUser;
@@ -28,14 +31,83 @@ const Profile = () => {
     }
   };
 
+  const UserPhoto = () => {
+    return user.photoURL ? (
+      <Image
+        style={styles.profileImage}
+        source={require("../../../assets/profile/profile-photo.png")}
+      />
+    ) : (
+      <MaterialIcons name={"person"} size={30} color={"gray"} />
+    );
+  };
+
+  const navigateToEditProfile = () => {
+    navigation.navigate("EditProfile");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>{user.displayName}</Text>
-      <CustomButton
-        buttonText={"Log out"}
-        backgroundColor={"red"}
-        onPress={userSignOut}
-      />
+      <View style={styles.profileContainer}>
+        <View style={styles.profileImageContainer}>
+          <UserPhoto />
+        </View>
+        <View style={styles.profileTextContainer}>
+          <Text style={styles.profileName}>{user.displayName}</Text>
+          <Text style={styles.profileEmail}>{user.email}</Text>
+        </View>
+      </View>
+
+      <View style={styles.generalContainer}>
+        <Text style={styles.titleText}>General</Text>
+        <AccountButton
+          iconName={"person"}
+          iconColor={"#4286FF"}
+          iconBackgroundColor={"#EDF2FF"}
+          size={20}
+          title={"Edit Profile"}
+          description={"Change profile picture, number, E-mail"}
+          onPress={navigateToEditProfile}
+        />
+        <AccountButton
+          iconName={"lock"}
+          iconColor={"#4286FF"}
+          iconBackgroundColor={"#EDF2FF"}
+          size={20}
+          title={"Change Password"}
+          description={"Update and strengthen account security"}
+        />
+      </View>
+      <View style={styles.preferencesContainer}>
+        <Text style={styles.titleText}>Preferences</Text>
+        <ToggleButton
+          iconName={"notifications"}
+          iconColor={"#4286FF"}
+          iconBackgroundColor={"#EDF2FF"}
+          size={20}
+          title={"Notification"}
+          description={"Customize your notication prefecenses"}
+        />
+        {/* icon adı değiştirilecek */}
+        <ToggleButton
+          iconName={"nights-stay"}
+          iconColor={"#4286FF"}
+          iconBackgroundColor={"#EDF2FF"}
+          size={20}
+          title={"Dark Mode"}
+          description={"Customize your theme"}
+        />
+        <AccountButton
+          iconName={"logout"}
+          iconColor={"#CE1040"}
+          iconBackgroundColor={"#FFF1F5"}
+          size={20}
+          title={"Log Out"}
+          titleColor={"#CE1040"}
+          description={"Securely log out of Account"}
+          onPress={userSignOut}
+        />
+      </View>
     </View>
   );
 };
--- src/pages/Profile/Profile.style.js
+++ src/pages/Profile/Profile.style.js
@@ -1,9 +1,66 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+
+const deviceSize = Dimensions.get("window");
 
 export default styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
+    paddingHorizontal: 30,
+    backgroundColor: "#F4F4F4",
+  },
+  profileContainer: {
+    backgroundColor: "white",
+    width: (deviceSize.width / 10) * 9,
+    flexDirection: "row",
+    paddingVertical: 10,
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    gap: 10,
+  },
+  profileImageContainer: {
+    borderRadius: 50,
+    width: 50,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F4F4F4",
+    borderRadius: 25
+  },
+  profileImage: {
+    borderRadius: 50,
+    width: 75,
+    height: 75,
+  },
+  profileTextContainer: {
+    justifyContent: "center",
+    gap: 5,
+  },
+  profileName: {
+    fontWeight: "700",
+  },
+  profileEmail: {
+    color: "gray",
+  },
+  generalContainer: {
+    backgroundColor: "white",
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    maxWidth: (deviceSize.width / 10) * 9,
+    paddingBottom: 20,
+  },
+  titleText: {
+    fontSize: 20,
+    color: "gray",
+    paddingVertical: 10,
+  },
+  preferencesContainer: {
+    backgroundColor: "white",
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    maxWidth: (deviceSize.width / 10) * 9,
+    paddingBottom: 20,
+  },
+});
