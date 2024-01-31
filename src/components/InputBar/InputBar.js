f2a7e8d2be648650950389f825068ef9f2174daa
bilalseen <bilalsen2010@gmail.com>
2024-01-31 21:26:46 +0300

    backgroundColor and color props added the component

--- src/components/InputBar/InputBar.js
+++ src/components/InputBar/InputBar.js
@@ -2,13 +2,20 @@ import React from "react";
 import { View, Text, TextInput } from "react-native";
 import styles from "./InputBar.style";
 
-const SearchBar = ({ placeholder, onChange, value }) => {
+const SearchBar = ({
+  placeholder,
+  onChange,
+  value,
+  backgroundColor,
+  color,
+}) => {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: backgroundColor }]}>
       <TextInput
         placeholder={placeholder}
         onChangeText={onChange}
         value={value}
+        style={[styles.input, { color: color }]}
       />
     </View>
   );
