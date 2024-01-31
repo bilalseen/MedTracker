625372fce177ecfbb450d2dafe46b7a881eb4cc9
bilalseen <bilalsen2010@gmail.com>
2024-01-31 21:27:33 +0300

    page edited

--- src/pages/EditProfile/EditProfile.js
+++ src/pages/EditProfile/EditProfile.js
@@ -1,13 +1,120 @@
-import { View, Text, Image } from "react-native";
-import React from "react";
+import { View, Image, Text, ToastAndroid } from "react-native";
+import React, { useState, useEffect } from "react";
+import styles from "./EditProfile.style";
+import CustomButton from "../../components/CustomButton";
+import FIREBASE_AUTH from "../../services/config";
+import MaterialIcons from "../../components/MaterialIcons";
+import { Formik } from "formik";
+import InputBar from "../../components/InputBar";
+import { updateProfile } from "firebase/auth";
 
-const EditProfile = () => {
-  return (
-    <View>
-      <View>
-        <Image />
-        
+const EditProfile = ({ navigation }) => {
+  const auth = FIREBASE_AUTH;
+  const [user, setUser] = useState(auth.currentUser);
+
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, [auth.currentUser]);
+
+  const UserPhoto = () => {
+    return user.photoURL ? (
+      <Image
+        style={styles.profileImage}
+        source={require("../../../assets/profile/profile-photo.png")}
+      />
+    ) : (
+      <View style={styles.profileImageContainer}>
+        <MaterialIcons name={"person"} size={50} color={"gray"} />
       </View>
+    );
+  };
+
+  const updateUserProfile = async (values) => {
+    try {
+      await updateProfile(user, {
+        displayName: values.displayName,
+        email: values.email,
+        photoURL: user.photoURL,
+      });
+
+      if (values.phoneNumber && values.phoneNumber !== user.phoneNumber) {
+        const credential = firebase.auth.PhoneAuthProvider.credential(
+          verificationId,
+          verificationCode
+        );
+        await user.updatePhoneNumber(credential);
+      }
+
+      console.log("User profile updated successfully:", user);
+
+      navigateToProfile();
+      ToastAndroid.show("Profile updated!", ToastAndroid.SHORT);
+    } catch (updateError) {
+      console.error("Error updating user profile:", updateError);
+    }
+  };
+
+  const navigateToProfile = () => {
+    navigation.navigate("TabNavigation");
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <UserPhoto />
+        <CustomButton
+          buttonText={"Change profile picture"}
+          backgroundColor={"white"}
+          fontSize={10}
+          color={"gray"}
+        />
+      </View>
+
+      <Formik
+        initialValues={{
+          displayName: user.displayName,
+          email: user.email,
+          phoneNumber: user.phoneNumber,
+        }}
+        onSubmit={(values) => {
+          updateUserProfile(values);
+          console.log(values);
+        }}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View style={styles.inputContainer}>
+            <View>
+              <Text style={styles.titleText}>Edit Profile</Text>
+            </View>
+            <InputBar
+              value={values.displayName}
+              backgroundColor={"white"}
+              onChange={handleChange("displayName")}
+            />
+            <InputBar
+              value={values.email}
+              backgroundColor={"white"}
+              onChange={handleChange("email")}
+            />
+            <InputBar
+              value={values.phoneNumber}
+              backgroundColor={"white"}
+              onChange={handleChange("phoneNumber")}
+              placeholder={values.phoneNumber ? "" : "Add your phone number"}
+            />
+            <View style={styles.buttonContainer}>
+              <CustomButton
+                buttonText={"Update Profile"}
+                onPress={handleSubmit}
+                backgroundColor="black"
+                color={"white"}
+                width={240}
+                height={50}
+              />
+            </View>
+          </View>
+        )}
+      </Formik>
     </View>
   );
 };
--- src/pages/EditProfile/EditProfile.style.js
+++ src/pages/EditProfile/EditProfile.style.js
@@ -1,3 +1,46 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
-export default styles = StyleSheet.create({});
+const deviceSize = Dimensions.get("window");
+
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  profileContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  profileImageContainer: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: "#E2E3E5",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  inputContainer: {
+    gap: 30,
+    marginTop: 50,
+    paddingVertical: 50,
+    backgroundColor: "white",
+    alignItems: "center",
+    width: (deviceSize.width / 10) * 9,
+    borderRadius: 20,
+  },
+  titleText: {
+    fontSize: 20,
+    color: "gray",
+    paddingVertical: 10,
+  },
+  buttonContainer: {
+    marginTop: 70,
+  },
+});
