6dbd689d73030ca6206eefa8475d2214078709bf
bilalseen <bilalsen2010@gmail.com>
2024-02-05 19:45:03 +0300

    getMedicineData, deleteMedicine func added and firestore connected

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,51 +1,99 @@
-import { View, Text, FlatList } from "react-native";
+import { View, FlatList } from "react-native";
 import React, { useEffect, useState } from "react";
 import styles from "./Home.style";
 import SearchBar from "../../components/SearchBar";
 import LoadingAnimation from "../../components/LoadingAnimation";
 import ErrorAnimation from "../../components/ErrorAnimation";
-import MedData from "../../../data.json";
 import MedCard from "../../components/MedCard";
+import {
+  db,
+  collection,
+  getDocs,
+  doc,
+  deleteDoc,
+} from "../../services/dbConfig";
+import FIREBASE_AUTH from "../../services/config";
+import { onAuthStateChanged } from "@firebase/auth";
 
-const Home = () => {
+const Home = ({ navigation }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const [data, setData] = useState(MedData);
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    try {
-    } catch (error) {
-      console.error("Veri yükleme hatası:", error);
-      setError(true);
-    } finally {
-      setLoading(false);
+  onAuthStateChanged(FIREBASE_AUTH, (authUser) => {
+    setUser(authUser);
+  });
+
+  const getMedicineData = async () => {
+    if (user) {
+      try {
+        const querySnapshot = await getDocs(collection(db, user.uid));
+        const newData = [];
+        querySnapshot.forEach((doc) => {
+          newData.push({
+            ...doc.data(),
+            id: doc.id,
+          });
+        });
+        setData(newData);
+        setError(false);
+      } catch (error) {
+        console.error("Veri yükleme hatası:", error);
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
     }
-  }, []);
+  };
+
+  const deleteMedicine = async (item) => {
+    try {
+      await deleteDoc(doc(db, user.uid, item.id));
+      await getMedicineData();
+    } catch (err) {
+      console.log("İlaç silme hatası: " + err);
+    }
+  };
 
   useEffect(() => {
-    setData(MedData);
-  }, [MedData]);
+    if (user) {
+      getMedicineData();
+    }
+  }, [user, data]);
 
-  if (loading) {
-    return <LoadingAnimation />;
-  }
   if (error) {
     return <ErrorAnimation />;
   }
 
-  const MedRenderItem = ({ item }) => {
-    return <MedCard item={item} />;
+  const navigateToEditMedicine = (item) => {
+    navigation.navigate("EditMedicine", { item });
   };
 
   return (
     <View style={styles.container}>
-      <SearchBar placeholder={"Search med..."} onSearch={() => null} />
-      <FlatList
-        data={data}
-        renderItem={({ item }) => <MedRenderItem item={item} />}
-        showsVerticalScrollIndicator={false}
-        style={styles.medFlatList}
+      <SearchBar
+        placeholder={"Search med..."}
+        onSearch={(text) => setSearch(text)}
       />
+      {loading ? (
+        <LoadingAnimation />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <MedCard
+              item={item}
+              onDeletePress={deleteMedicine}
+              onNavigatePress={navigateToEditMedicine}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+          style={styles.medFlatList}
+        />
+      )}
     </View>
   );
 };
