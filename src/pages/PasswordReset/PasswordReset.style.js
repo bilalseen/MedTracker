835b2ca0de56e1cc7229ffdb27581dc1053b32e0
bilalseen <bilalsen2010@gmail.com>
2024-02-05 22:53:13 +0300

    style defined

--- src/pages/PasswordReset/PasswordReset.style.js
+++ src/pages/PasswordReset/PasswordReset.style.js
@@ -1,3 +1,15 @@
 import { StyleSheet } from "react-native";
 
-export default styles = StyleSheet.create({});
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 50,
+  },
+  forgotPasswordAnimation: {
+    width: 300,
+    height: 300,
+    zIndex: 1,
+  },
+});
