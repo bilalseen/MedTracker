b3efb44204a8c5912735f18aee2a399e2db9bda1
bilalseen <bilalsen2010@gmail.com>
2024-02-05 21:23:47 +0300

    password reset page added the router

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TabNavigation from "../routes/TabNavigation";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import PasswordReset from "../pages/PasswordReset";
 import EditProfile from "../pages/EditProfile";
 import EditMedicine from "../pages/EditMedicine";
 import PasswordChange from "../pages/PasswordChange";
@@ -53,6 +54,11 @@ function Router() {
           <Stack.Screen
             name="SignUp"
             component={SignUp}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="PasswordReset"
+            component={PasswordReset}
             options={{ headerShown: false }}
           />
         </Stack.Navigator>
