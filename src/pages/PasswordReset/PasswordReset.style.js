3b79a45d4c819684b5153fb308649406e456e044
bilalseen <bilalsen2010@gmail.com>
2024-02-06 08:05:26 +0300

    style edited

--- src/pages/PasswordReset/PasswordReset.style.js
+++ src/pages/PasswordReset/PasswordReset.style.js
@@ -12,4 +12,35 @@ export default styles = StyleSheet.create({
     height: 300,
     zIndex: 1,
   },
+  findPeopleAnimation: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headerContainer: {
+    alignItems: "flex-start",
+    gap: 10,
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: "600",
+    width: 200,
+    color: "#213060",
+  },
+  descriptionText: {
+    fontSize: 18,
+    width: 300,
+    color: "#213060",
+  },
+  passwordResetText: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#2F2E41",
+  },
+  resendContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  resendText: { color: "#213060", fontWeight: "600" },
 });
