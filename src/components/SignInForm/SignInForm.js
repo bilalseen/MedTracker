5ecd5f14e02ae63cac7de9752341eaf6b0b6b7a4
bilalseen <bilalsen2010@gmail.com>
2024-02-06 18:19:42 +0300

    yup added the forms

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import styles from "./SignInForm.style";
 import { Formik } from "formik";
@@ -7,51 +7,101 @@ import InputBar from "../InputBar";
 import CustomButton from "../CustomButton";
 import FIREBASE_AUTH from "../../services/config";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import * as Yup from "yup";
 
-const signInUser = (values) => {
-  const auth = FIREBASE_AUTH;
-  signInWithEmailAndPassword(auth, values.email, values.password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      console.log(user.email);
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-};
+const SignInForm = ({ gap, onPress, buttonBackgroundColor }) => {
+  const [firebaseError, setFirebaseError] = useState("");
 
-const SignInForm = ({
-  gap,
-  onPress,
-  buttonBackgroundColor,
-  buttonTextColor,
-}) => {
-  const onSubmit = (values) => {
-    signInUser(values);
+  const onSubmit = (values, setFieldError) => {
+    signInUser(values, setFirebaseError, setFieldError);
   };
+
+  const signInUser = (values, setFirebaseError, setFieldError) => {
+    const auth = FIREBASE_AUTH;
+    signInWithEmailAndPassword(auth, values.email, values.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user.email);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setFirebaseError(error.code);
+        // Hata oluştuğunda ilgili alanın 'touched' durumunu set etmek için setFieldTouched kullanılmalı
+        setFieldError("email", true);
+        setFieldError("password", true);
+      });
+  };
+
   return (
-    <Formik initialValues={{ email: "", password: "" }} onSubmit={onSubmit}>
-      {({ handleChange, handleBlur, handleSubmit, values }) => (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={onSubmit}
+      validationSchema={Yup.object().shape({
+        email: Yup.string()
+          .email("Geçerli bir e-posta giriniz")
+          .required("E-posta alanı boş bırakılamaz"),
+        password: Yup.string()
+          .min(6, "Şifreniz en az 6 karakter olmalıdır")
+          .required("Şifre alanı boş bırakılamaz"),
+      })}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        touched,
+        setFieldTouched,
+        setFieldError, // Formik 2.x sürümünde setFieldError da kullanılabilir
+      }) => (
         <View style={[styles.inputContainer, { gap: gap }]}>
-          <InputBar
-            onChange={handleChange("email")}
-            onBlur={handleBlur("email")}
-            placeholder={"Email "}
-            keyboardType={"email-address"}
-            backgroundColor={"white"}
-            borderColor={"#EFEFEF"}
-          />
-          <AuthInput
-            onChangeText={handleChange("password")}
-            onBlur={handleBlur("password")}
-            placeholder={"Password"}
-            secureTextEntry={true}
-            backgroundColor={"white"}
-            borderColor={"#EFEFEF"}
-          />
+          <View>
+            <InputBar
+              onChange={handleChange("email")}
+              onBlur={() => {
+                handleBlur("email");
+                setFieldTouched("email", true); // Hata olduğunda touched durumunu set et
+              }}
+              placeholder={"Email "}
+              keyboardType={"email-address"}
+              backgroundColor={"white"}
+              borderColor={touched.email && errors.email ? "red" : "#EFEFEF"}
+            />
+            {touched.email && errors.email ? (
+              <Text style={styles.warningText}>{errors.email}</Text>
+            ) : null}
+          </View>
+          <View>
+            <AuthInput
+              onChangeText={(text) => {
+                handleChange("password")(text);
+                setFirebaseError("");
+              }}
+              onBlur={() => {
+                handleBlur("password");
+                setFieldTouched("password", true); // Hata olduğunda touched durumunu set et
+              }}
+              placeholder={"Password"}
+              secureTextEntry={true}
+              backgroundColor={"white"}
+              borderColor={
+                (touched.password && errors.password) ||
+                firebaseError.length > 0
+                  ? "red"
+                  : "#EFEFEF"
+              }
+            />
+            {touched.password && errors.password ? (
+              <Text style={styles.warningText}>{errors.password}</Text>
+            ) : null}
+
+            {firebaseError ? (
+              <Text style={styles.warningText}>{firebaseError}</Text>
+            ) : null}
+          </View>
+
           <TouchableOpacity onPress={onPress}>
             <Text style={styles.passwordForgotText}>Forgot password?</Text>
           </TouchableOpacity>
@@ -59,7 +109,7 @@ const SignInForm = ({
             buttonText={"Sign In"}
             onPress={handleSubmit}
             backgroundColor={buttonBackgroundColor}
-            buttonTextColor={buttonTextColor}
+            buttonTextColor={"white"}
             width={240}
             height={50}
           />
--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,5 +1,5 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SignUpForm.style";
 import { Formik } from "formik";
 import AuthInput from "../AuthInput";
@@ -7,74 +7,132 @@ import InputBar from "../InputBar";
 import CustomButton from "../CustomButton";
 import FIREBASE_AUTH from "../../services/config";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-
-const signUpUser = (values) => {
-  const auth = FIREBASE_AUTH;
-  createUserWithEmailAndPassword(auth, values.email, values.password)
-    .then((userCredential) => {
-      // Signed up
-      const user = userCredential.user;
-      console.log(user.displayName);
-      updateProfileAndLog(user, values);
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // Handle error
-    });
-};
-
-const updateProfileAndLog = async (user, values) => {
-  try {
-    await updateProfile(user, {
-      displayName: values.fullName,
-    });
-    console.log("Profil güncellendi:", user.displayName);
-  } catch (error) {
-    console.error("Profil güncelleme hatası:", error.message);
-  }
-};
+import * as Yup from "yup";
 
 const SignUpForm = ({ gap, buttonBackgroundColor, buttonTextColor }) => {
+  const [firebaseError, setFirebaseError] = useState("");
+
   const onSubmit = (values) => {
     signUpUser(values);
   };
+  const signUpUser = (values) => {
+    const auth = FIREBASE_AUTH;
+    createUserWithEmailAndPassword(auth, values.email, values.password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user.displayName);
+        updateProfileAndLog(user, values);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setFirebaseError(error.code);
+
+        console.log(error.code);
+        console.log(error.message);
+        // Handle error
+      });
+  };
+
+  const updateProfileAndLog = async (user, values) => {
+    try {
+      await updateProfile(user, {
+        displayName: values.fullName,
+      });
+      console.log("Profil güncellendi:", user.displayName);
+    } catch (error) {
+      console.error("Profil güncelleme hatası:", error.message);
+    }
+  };
+
   return (
     <Formik
       initialValues={{ fullName: "", email: "", password: "" }}
       onSubmit={onSubmit}
+      validationSchema={Yup.object().shape({
+        fullName: Yup.string().required("İsim alanı boş bırakılamaz!"),
+        email: Yup.string()
+          .email("Geçerli bir e-posta giriniz")
+          .required("E-posta alanı boş bırakılamaz"),
+        password: Yup.string()
+          .min(6, "Şifreniz en az 6 karakter olmalıdır")
+          .required("Şifre alanı boş bırakılamaz"),
+      })}
     >
-      {({ handleChange, handleBlur, handleSubmit, values }) => (
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        touched,
+        setFieldTouched,
+        setFieldError,
+      }) => (
         <View style={[styles.inputContainer, { gap: gap }]}>
-          <InputBar
-            onChange={handleChange("fullName")}
-            onBlur={handleBlur("fullName")}
-            value={values.firstName}
-            placeholder={"Full Name"}
-            backgroundColor={"white"}
-            borderColor={"#EFEFEF"}
-          />
-          <InputBar
-            onChange={handleChange("email")}
-            onBlur={handleBlur("email")}
-            placeholder={"Email "}
-            keyboardType={"email-address"}
-            backgroundColor={"white"}
-            borderColor={"#EFEFEF"}
-          />
-          <AuthInput
-            onChangeText={handleChange("password")}
-            onBlur={handleBlur("password")}
-            placeholder={"Password"}
-            secureTextEntry={true}
-            backgroundColor={"white"}
-            borderColor={"#EFEFEF"}
-          />
+          <View>
+            <InputBar
+              onChange={handleChange("fullName")}
+              onBlur={handleBlur("fullName")}
+              value={values.firstName}
+              placeholder={"Full Name"}
+              backgroundColor={"white"}
+              borderColor={"#EFEFEF"}
+            />
+            {touched.fullName && errors.fullName ? (
+              <Text style={styles.warningText}>{errors.fullName}</Text>
+            ) : null}
+          </View>
+          <View>
+            <InputBar
+              onChange={(text) => {
+                handleChange("email")(text);
+                setFirebaseError("");
+              }}
+              onBlur={handleBlur("email")}
+              placeholder={"Email "}
+              keyboardType={"email-address"}
+              backgroundColor={"white"}
+              borderColor={
+                firebaseError === "auth/invalid-email" ||
+                firebaseError === "auth/email-already-in-use"
+                  ? "red"
+                  : "#EFEFEF"
+              }
+            />
+            {touched.email && errors.email ? (
+              <Text style={styles.warningText}>{errors.email}</Text>
+            ) : null}
+
+            {firebaseError === "auth/invalid-email" ||
+            firebaseError === "auth/email-already-in-use" ? (
+              <Text style={styles.warningText}>{firebaseError}</Text>
+            ) : null}
+          </View>
+          <View>
+            <AuthInput
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              placeholder={"Password"}
+              secureTextEntry={true}
+              backgroundColor={"white"}
+              borderColor={
+                (touched.password && errors.password) ||
+                firebaseError.length > 0
+                  ? "red"
+                  : "#EFEFEF"
+              }
+            />
+            {touched.password && errors.password ? (
+              <Text style={styles.warningText}>{errors.password}</Text>
+            ) : null}
+          </View>
           <CustomButton
             buttonText={"Sign Up"}
             onPress={handleSubmit}
             backgroundColor={buttonBackgroundColor}
-            buttonTextColor={buttonTextColor}
+            buttonTextColor={"white"}
             width={240}
             height={50}
           />
