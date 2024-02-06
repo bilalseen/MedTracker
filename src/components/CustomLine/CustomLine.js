d126464fea8bb5d17408f6972549634559f4f41f
bilalseen <bilalsen2010@gmail.com>
2024-02-06 11:03:14 +0300

    cutom line component container margin vertical variable added the props

--- src/components/CustomLine/CustomLine.js
+++ src/components/CustomLine/CustomLine.js
@@ -2,9 +2,20 @@ import { View, Text } from "react-native";
 import React from "react";
 import styles from "./CustomLine.style";
 
-const CustomLine = ({ text, textColor, borderColor, width }) => {
+const CustomLine = ({
+  text,
+  textColor,
+  borderColor,
+  width,
+  marginVertical,
+}) => {
   return (
-    <View style={[styles.container, { width: width }]}>
+    <View
+      style={[
+        styles.container,
+        { width: width, marginVertical: marginVertical },
+      ]}
+    >
       <View style={[styles.line, { borderColor: borderColor }]} />
       <Text style={[styles.text, { color: textColor }]}>{text}</Text>
       <View style={[styles.line, { borderColor: borderColor }]} />
