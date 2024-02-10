e84b8df65e9c17442c7431114bb171920eda51b6
bilalseen <bilalsen2010@gmail.com>
2024-02-10 23:17:03 +0300

    loading animation added

--- src/pages/Add/Add.js
+++ src/pages/Add/Add.js
@@ -10,11 +10,14 @@ import styles from "./Add.style";
 import InputBar from "../../components/InputBar";
 import CustomCalendar from "../../components/CustomCalendar";
 import CustomButton from "../../components/CustomButton";
+import LoadingAnimation from "../../components/LoadingAnimation";
+import AwesomeAlert from "react-native-awesome-alerts";
 
 const Add = () => {
   const [buttonColor, setButtonColor] = useState("gray");
   const [inputText, setInputText] = useState("");
   const [selectedDate, setSelectedDate] = useState("");
+  const [loading, setLoading] = useState(false);
   const today = moment();
 
   const [user, setUser] = useState(null);
@@ -25,6 +28,7 @@ const Add = () => {
 
   const addMedicine = async () => {
     if (user) {
+      setLoading(true);
       try {
         const docRef = await addDoc(collection(db, user.uid), {
           name: inputText,
@@ -35,6 +39,7 @@ const Add = () => {
         setSelectedDate("");
         console.log("Document written with ID: ", docRef.id);
         ToastAndroid.show("Your medicine has been added!", ToastAndroid.SHORT);
+        setLoading(false);
       } catch (e) {
         console.error("Error adding document: ", e);
         ToastAndroid.show(e.message, ToastAndroid.SHORT);
@@ -54,6 +59,9 @@ const Add = () => {
     const formattedDate = moment(date).format("YYYY-MM-DD").toString();
     setSelectedDate(formattedDate);
   };
+  if (loading) {
+    return <AwesomeAlert show={loading ? true : false} showProgress={true} />;
+  }
 
   return (
     <View style={styles.container}>
