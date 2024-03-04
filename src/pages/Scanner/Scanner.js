fa3a0ca0f00d6a7c933a748af1a5b510abe31dc8
bilalseen <bilalsen2010@gmail.com>
2024-03-04 22:11:29 +0300

    page created

---
+++ src/pages/Scanner/Scanner.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import styles from "./Scanner.style";
+
+export default function Scanner() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Scanner Page!!</Text>
+    </View>
+  );
+}
